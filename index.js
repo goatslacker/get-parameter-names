5a02b66924dcd3e8b4ecb563a4a722fa479c0a0e
Allain Lalonde <allain.lalonde@gmail.com>
2016-03-18 11:18:33 -0400

    Adding support for some ES2015 features

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+var DEFAULT_PARAMS = /=[^,]+/mg;
+var FAT_ARROWS = /=>.*$/mg;
+
 function getParameterNames(fn) {
-  var code = fn.toString().replace(COMMENTS, '');
+  var code = fn.toString()
+    .replace(COMMENTS, '')
+    .replace(FAT_ARROWS, '')
+    .replace(DEFAULT_PARAMS, '');
+
   var result = code.slice(code.indexOf('(') + 1, code.indexOf(')'))
     .match(/([^\s,]+)/g);
 
